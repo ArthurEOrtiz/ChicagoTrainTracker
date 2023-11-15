5b48a620dcd0376a5d74fd2c28a15420db80d19d
Arthur E Ortiz <100542148+ArthurEOrtiz@users.noreply.github.com>
2023-11-14 17:01:26 -0700

    Set up station view view model

---
+++ ChicagoTrainTracker/ViewModel/StationViewViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace ChicagoTrainTracker.ViewModel
+{
+	public class StationViewViewModel : ViewModelBase
+	{
+		private string _stationName = "Hello World!";
+
+		public string StationName
+		{
+			get => _stationName;
+			set
+			{
+				_stationName = value;
+				OnPropertyChanged(nameof(StationName));
+			}
+		}
+
+		public StationViewViewModel() { }
+
+	}
+}
