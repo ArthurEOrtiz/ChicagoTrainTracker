f669d515aa815c980ab7cbea73f342bc3ff9e03e
Arthur E Ortiz <100542148+ArthurEOrtiz@users.noreply.github.com>
2023-11-16 09:20:09 -0700

    clean code

---
+++ ChicagoTrainTracker/ViewModel/EtaViewModel.cs
@@ -0,0 +1,23 @@
+﻿using ChicagoTrainTracker.Model;
+using System;
+
+namespace ChicagoTrainTracker.ViewModel
+{
+	public class EtaViewModel : ViewModelBase
+	{
+		private readonly Eta _etaData;
+
+		public string Route => _etaData.Route;
+		public int RouteNumber => _etaData.RouteNumber;
+		public string StopDescription => _etaData.StopDescription;
+		public string DestinationName => _etaData.DestinationName;
+		public DateTime ArrivalTime => _etaData.ArrivalTime;
+		public int IsApproaching => _etaData.IsApproaching;
+		public int IsDelayed => _etaData.IsDelayed;
+
+		public EtaViewModel(Eta etaData)
+		{
+			_etaData = etaData;
+		}
+	}
+}
--- ChicagoTrainTracker/ViewModel/StationViewViewModel.cs
+++ ChicagoTrainTracker/ViewModel/StationViewViewModel.cs
@@ -1,7 +1,12 @@
 ﻿using ChicagoTrainTracker.Commands;
+using ChicagoTrainTracker.Model;
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace ChicagoTrainTracker.ViewModel
 {
@@ -18,11 +23,15 @@ namespace ChicagoTrainTracker.ViewModel
 			}
 		}
 
+		private readonly ObservableCollection<EtaViewModel> _eTAData;
+		public IEnumerable<EtaViewModel> ETAData => _eTAData;
+
 		public RelayCommand ChangeStationNameCommand { get; private set; }
 
 		public StationViewViewModel()
 		{
 			ChangeStationNameCommand = new RelayCommand(ChangeStationName);
+			_eTAData = new ObservableCollection<EtaViewModel>();
 		}
 
 		private async void ChangeStationName(object parameter)
@@ -40,10 +49,61 @@ namespace ChicagoTrainTracker.ViewModel
 
 			using (var httpClient = new HttpClient())
 			{
-				string apiUrl = $"http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key{ctaApiKey}&mapid={Station.MapId}";
-				var response = await httpClient.GetAsync(apiUrl);
+				string apiUrl = $"http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key={ctaApiKey}&mapid={Station.MapId}";
+				var response = await httpClient.GetStringAsync(apiUrl);
+
+				ParserApiResponse(response);
 
 			}
+		}
+
+		private void ParserApiResponse(string xmlResponse)
+		{
+			var xmlDoc = new XmlDocument();
+			xmlDoc.LoadXml(xmlResponse);
+
+			bool isResponseGood = CheckErrorCode(xmlDoc);
+
+			if (isResponseGood)
+			{
+				SetEtaData(xmlDoc);
+			}
+		}
+
+		private void SetEtaData(XmlDocument xmlDoc)
+		{
+			_eTAData.Clear();
+			var etaNodes = xmlDoc.SelectNodes("//eta");
+
+			foreach (XmlNode etaNode in etaNodes)
+			{
+				var etaData = new Eta
+				{
+					Route = etaNode.SelectSingleNode("rt")?.InnerText,
+					RouteNumber = int.TryParse(etaNode.SelectSingleNode("rn")?.InnerText, out int parsedRouteNumber) ? parsedRouteNumber : 0,
+					StopDescription = etaNode.SelectSingleNode("stpDe")?.InnerText,
+					DestinationName = etaNode.SelectSingleNode("destNm")?.InnerText,
+					ArrivalTime = DateTime.TryParseExact(etaNode.SelectSingleNode("arrT")?.InnerText, "yyyyMMdd HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out var arrivalTime) ? arrivalTime
+								: default(DateTime),
+					IsApproaching = int.TryParse(etaNode.SelectSingleNode("isApp")?.InnerText, out int parsedIsApproaching) ? parsedIsApproaching : 0,
+					IsDelayed = int.TryParse(etaNode.SelectSingleNode("isDly")?.InnerText, out int parsedIsDelayed) ? parsedIsDelayed : 0
+				};
+
+				_eTAData.Add(new EtaViewModel(etaData));
+			}
+		}
+
+		private bool CheckErrorCode(XmlDocument xmlDoc)
+		{
+			int errorCode = int.TryParse(xmlDoc.SelectSingleNode("//errCd")?.InnerText, out int parseErrorCode) ? parseErrorCode : 0;
+			string errorMessage = xmlDoc.SelectSingleNode("//errNm")?.InnerText;
+
+			if (errorCode != 0)
+			{
+				//throw new ApiErrorException($"API Error (Code: {errorCode}): {errorMessage}");
+			}
+
+			return errorCode == 0;
 		}
 	}
 }
